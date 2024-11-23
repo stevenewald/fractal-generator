3045f70296d627e6113a39a883d425e2f586d848
Max Glass <glassmax13@gmail.com>
2024-11-23 16:30:42 -0600

    add resizing to display

--- source/graphics/basic_display.cpp
+++ source/graphics/basic_display.cpp
@@ -1,6 +1,8 @@
 #include "basic_display.hpp"
 
+#include <fmt/format.h>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 namespace fractal {
 void BasicDisplay::set_pixel(std::size_t x_pos, std::size_t y_pos, uint8_t value)
@@ -31,9 +33,53 @@ void BasicDisplay::display_window()
 
     sf::Sprite sprite(texture);
 
+    bool left_mouse_down{};
+    float selection_start_x{};
+    float selection_start_y{};
+    float mouse_x{};
+    float mouse_y{};
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::MouseMoved:
+                    mouse_x = static_cast<float>(event.mouseMove.x);
+                    mouse_y = static_cast<float>(event.mouseMove.y);
+                    break;
+
+                case sf::Event::MouseButtonPressed:
+                    if (event.mouseButton.button != sf::Mouse::Left) {
+                        break;
+                    }
+                    left_mouse_down = true;
+                    selection_start_x = static_cast<float>(event.mouseButton.x);
+                    selection_start_y = static_cast<float>(event.mouseButton.y);
+                    break;
+
+                case sf::Event::MouseButtonReleased:
+                    if (event.mouseButton.button != sf::Mouse::Left) {
+                        break;
+                    }
+                    on_resize_(
+                        sf::Vector2f(
+                            std::min(mouse_x, selection_start_x),
+                            std::min(mouse_y, selection_start_y)
+                        ),
+                        sf::Vector2f(
+                            std::max(mouse_x, selection_start_x),
+                            std::max(mouse_y, selection_start_y)
+                        )
+                    );
+                    left_mouse_down = false;
+                    break;
+
+                default:
+                    break;
+            }
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
@@ -42,6 +88,19 @@ void BasicDisplay::display_window()
         window.clear(sf::Color::Black);
 
         window.draw(sprite);
+        if (left_mouse_down) {
+            sf::RectangleShape selection_rectangle(sf::Vector2f(
+                std::abs(mouse_x - selection_start_x),
+                std::abs(mouse_y - selection_start_y)
+            ));
+            selection_rectangle.setPosition(
+                std::min(mouse_x, selection_start_x),
+                std::min(mouse_y, selection_start_y)
+            );
+
+            selection_rectangle.setFillColor(sf::Color(255, 0, 0, 127));
+            window.draw(selection_rectangle);
+        }
 
         window.display();
     }
--- source/graphics/basic_display.hpp
+++ source/graphics/basic_display.hpp
@@ -2,15 +2,24 @@
 
 #include "config.hpp"
 
+#include <SFML/System/Vector2.hpp>
+
 #include <cstdint>
 
 #include <array>
+#include <functional>
+#include <utility>
 
 namespace fractal {
 class BasicDisplay {
     std::array<std::array<uint8_t, WINDOW_HEIGHT>, WINDOW_WIDTH> pixels_{};
+    std::function<void(sf::Vector2f, sf::Vector2f)> on_resize_;
 
 public:
+    explicit BasicDisplay(std::function<void(sf::Vector2f, sf::Vector2f)> on_resize) :
+        on_resize_(std::move(on_resize))
+    {}
+
     void set_pixel(std::size_t x_pos, std::size_t y_pos, std::uint8_t value);
     void display_window();
 };
