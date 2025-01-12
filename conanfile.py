38c72715396efdfc2a8dbf33077ca8ad021aa06c
Steven Ewald <stevenewald6@gmail.com>
2025-01-12 14:14:04 -0600

    Re-added boost but without LTO

--- conanfile.py
+++ conanfile.py
@@ -12,6 +12,10 @@ class Recipe(ConanFile):
         self.requires("fmt/11.0.2")
         self.requires("sfml/2.6.1")
         self.requires("argparse/3.1")
+        self.requires("boost/1.86.0")
+
+    def configure(self):
+        self.options["boost"].without_test=True
 
     def build_requirements(self):
         self.test_requires("catch2/3.7.0")
